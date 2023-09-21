bed833e904241b3a87351b9afd88f6d868062739
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2023-09-21 10:05:47 -0700

    user creatrion done

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,7 @@ coonectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const User = require('./../models/Users')
+const { body, validationResult } = require('express-validator')
 
-router.get('/', (req,res)=>{
-    obj = {
-        name: 'usman'
-    }
-    res.json(obj)
+
+router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+], (req, res) => {
+const errors = validationResult(req);
+if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array() });
+}  
+User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => res.json(user));
 })
 
 module.exports = router
