60d653740e2160fd18f84af081c5ac5496ff9f98
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2023-09-24 07:33:34 -0700

    user login api done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = "thisismyCode";
-
+// create a user 
 router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
@@ -43,6 +43,43 @@ router.post('/createuser', [
         // console.log(err);
         res.json({ error: "please ", message: error.message })
     }
+})
+
+// authenticate a user
+router.post('/login', [
+    body('email','Enter a valid email').isEmail(),
+    body('password', 'password cannot be blank').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+ // if there are errors return bad request
+ if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+}
+
+const {email , password} = req.body;
+
+try{
+    let user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({error:'enter correct credentials'})
+    }
+     const passCompare = await bcrypt.compare(password,user.password)
+     if(!passCompare){
+        return res.status(400).json({error:'enter correct credentials'})
+     }
+     const data = {
+        user:{
+            id : user.id
+        }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET)
+    res.json({authToken})
+
+} catch (error) { 
+    // console.log(err);
+    res.status(500).send({ error: "internal server error occured"})
+}
+
 })
 
 module.exports = router
